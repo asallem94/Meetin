c9cf4e4988472153bdfe454fb83cac228ab1cccc
Sallem Ahmed <asallem94@gmail.com>
2018-11-13 11:35:08 -0500

    initialize users table indatabase

---
+++ db/migrate/20181113160943_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, unique: true
+      t.string :name, null: false
+      t.string :profile_img_url
+      t.text :bio
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+
+
+  end
+end
