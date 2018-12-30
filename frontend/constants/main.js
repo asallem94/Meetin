b12a0812ea19b4f58b4cca8c53d450abc8ae009c
Sallem Ahmed <asallem94@gmail.com>
2018-12-30 01:59:35 -0500

    change ws path

--- frontend/constants/main.js
+++ frontend/constants/main.js
@@ -1,7 +1,7 @@
 // export const API_ROOT = 'http://localhost:3000';
 // export const API_WS_ROOT = 'ws://localhost:3000/cable';
 export const API_ROOT = 'https://meet-in.herokuapp.com';
-export const API_WS_ROOT = 'ws://https://meet-in.herokuapp.com/cable';
+export const API_WS_ROOT = 'ws://meet-in.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
